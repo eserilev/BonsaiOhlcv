763d817e2a9279fed0a0da3b3dab1af15c435932
Eitan Seri-Levi <eserilev@ucsc.edu>
2020-05-31 16:01:32 -0700

    log ohlcv data

---
+++ README.md
@@ -0,0 +1,12 @@
+Make sure you have pipenv installed
+
+pip install pipenv
+
+Install project dependencies
+
+pipenv install
+
+OR if you're having issues 
+
+python -m pipenv install
+
---
+++ ohlcv.py
@@ -0,0 +1,56 @@
+import datetime
+import mysql.connector
+import pymongo
+import ccxt
+
+
+# database stuff for later
+# client = pymongo.MongoClient("mongodb+srv://eserilev:$Pider37#@bonsaimongodb-wqutq.mongodb.net/test?retryWrites=true&w=majority")
+# bonsai_db = client.bonsai
+# ohlcvCollection = bonsai_db.ohlcvs
+
+# Bonsai implementation of
+# open high low close and volume (ohlcv)
+# data fetching and logging
+class BonsaiOhlcv():
+    
+    def __init__(self):
+        self.bitmex = ccxt.bitmex({
+            'rateLimit': 10000,
+            'enableRateLimit': True
+        })
+    
+    # grab the ohlcv of bitcoin 
+    # starting at from_date and 
+    # ending at the current date
+    # using the ccxt bitmex client
+    #
+    # returns: a list of ohlcv data
+    def get_ohlcv(self, from_date):
+        now = self.bitmex.milliseconds()
+        then = from_date.timestamp() * 1000
+        time_span = now - then
+        return self.bitmex.fetch_ohlcv('BTC/USD', '1d', time_span)
+
+    # todo: update our ohlcv mongodb collection with the data from ohlcv_list
+    # todo: create unique constraint on datestamp to prevent duplicate data
+    def update_ohlcv_logs(self, ohlcv_list):
+        print(len(ohlcv_list))
+        for ohlcv in ohlcv_list:
+            print(ohlcv)
+            #ohlcvCollection.insert_one(ohlcv)
+
+
+# example usage of using the BonsaiOhlcv class
+# to grab current ohlcv data and log it
+def main():
+    print('starting ohlcv calculations')
+    date_time_str = input("input a date: YYYY-MM-DD:\n")
+    date_range = datetime.datetime.strptime(date_time_str, '%Y-%m-%d')
+    bonsai_ohlcv = BonsaiOhlcv()
+    ohlcv_list = bonsai_ohlcv.get_ohlcv(date_range)
+    bonsai_ohlcv.update_ohlcv_logs(ohlcv_list)
+    
+if __name__ == "__main__":
+    main()
+   
